c2c1e5b697e5b50461e3bf8ed32ea1fbdccc755d
Resty <39333935+restyyow@users.noreply.github.com>
2022-01-19 15:06:50 +0800

    Solution 2

--- Solution_Exam1.sql
+++ Solution_Exam1.sql
@@ -2,7 +2,27 @@
 
 1. Show the top selling product in terms order quantity of sales per product category for each month
 
----------
+------------------------
+----- Final Answer -----
+------------------------
+
+begin
+	with cte_1 as (select ProductID, Name, ProductNumber, ProductSubcategoryID, SellStartDate, SellEndDate, datename(month,SellEndDate)+' '+datename(year,SellEndDate) as MonthlySales
+	from Production.Product)
+
+	select count(a.ProductID) as Product_Count_Sold, b.Name as Product_SubCategory_Name, c.Name as Product_Category_Name, a.MonthlySales from
+	cte_1 a left join Production.ProductSubcategory b on a.ProductSubcategoryID = b.ProductSubcategoryID
+	left join Production.ProductCategory c on c.ProductCategoryID = b.ProductCategoryID
+	where a.MonthlySales is not null 
+	group by b.Name,c.Name,a.MonthlySales
+	order by MonthlySales,Product_Count_Sold
+end
+go
+
+---------------------------
+--------- TESTING ---------
+---------------------------
+
 
 use AdventureWorks2008R2
 go
@@ -59,19 +79,3 @@ left join Production.ProductCategory c on c.ProductCategoryID = b.ProductCategor
 where a.SellEndDate is not null order by 1
 go
 
-------------------------
------ Final Answer -----
-------------------------
-
-begin
-	with cte_1 as (select ProductID, Name, ProductNumber, ProductSubcategoryID, SellStartDate, SellEndDate, datename(month,SellEndDate)+' '+datename(year,SellEndDate) as MonthlySales
-	from Production.Product)
-
-	select count(a.ProductID) as Product_Count_Sold, b.Name as Product_SubCategory_Name, c.Name as Product_Category_Name, a.MonthlySales from
-	cte_1 a left join Production.ProductSubcategory b on a.ProductSubcategoryID = b.ProductSubcategoryID
-	left join Production.ProductCategory c on c.ProductCategoryID = b.ProductCategoryID
-	where a.MonthlySales is not null 
-	group by b.Name,c.Name,a.MonthlySales
-	order by MonthlySales,Product_Count_Sold
-end
-go
